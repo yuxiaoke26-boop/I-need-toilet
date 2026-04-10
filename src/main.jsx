6e408764ab654d820571174c156341b2d0bca9cc
yuxiaoke26-boop <yuxiaoke26@gmail.com>
2026-04-10 19:38:22 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,7 @@ import App from './App.jsx'
 
 // 手动注册 Service Worker (原生 API 方式)
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // 指向我们刚才在 public 文件夹里创建的那个文件
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((err) => {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
-}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
